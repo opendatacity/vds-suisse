590ba51af747b45d3fec88aba169f8128b4a9f05
Micho <github@michael-kreil.de>
2014-03-27 15:34:10 +0100

    cleaning up

--- merge.js
+++ merge.js
@@ -4,7 +4,9 @@ var config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
 var events = [];
 
 // VDS einlesen
-require('vds').import('data/vds.tsv', events, config);
+var data = require('vds').import('../vds-data/vds/vds.tsv', config);
+//fs.writeFileSync('../vds-data/vds.json', JSON.stringify(data), 'utf8');
+fs.writeFileSync('../vds-data/vds.json', JSON.stringify(data, null, '\t'), 'utf8');
 
 
 
