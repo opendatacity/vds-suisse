7f1fa6cd3e526bc50ee4d2a6e3a22e436ba12321
Micho <github@michael-kreil.de>
2014-03-27 15:39:31 +0100

    adding "using cache" option

--- merge.js
+++ merge.js
@@ -1,12 +1,17 @@
 var fs = require('fs');
 var config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
 
-var events = [];
+var vds, events = [];
+
+var usingCache = false;
 
 // VDS einlesen
-var data = require('vds').import('../vds-data/vds/vds.tsv', config);
-//fs.writeFileSync('../vds-data/vds.json', JSON.stringify(data), 'utf8');
-fs.writeFileSync('../vds-data/vds.json', JSON.stringify(data, null, '\t'), 'utf8');
+if (usingCache) {
+	vds = JSON.parse(fs.readFileSync('../vds-data/vds.json', 'utf8'));
+} else {
+	vds = require('vds').import('../vds-data/vds/vds.tsv', config);
+	fs.writeFileSync('../vds-data/vds.json', JSON.stringify(vds, null, '\t'), 'utf8');
+}
 
 
 
