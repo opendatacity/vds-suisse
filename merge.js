2f34f3d52504597ddd36d4980283ea95051ebe34
Micho <github@michael-kreil.de>
2014-03-30 19:44:40 +0200

    adding logging

--- merge.js
+++ merge.js
@@ -8,8 +8,10 @@ var usingCache = true;
 // VDS einlesen
 var vdsJSON = config.cachePath + 'vds.json';
 if (usingCache && fs.existsSync(vdsJSON)) {
+	console.log('Load VDS from cache');
 	vds = JSON.parse(fs.readFileSync(vdsJSON, 'utf8'));
 } else {
+	console.log('Import VDS');
 	vds = require('vds').import(config.inputPath + 'vds/vds.tsv', config);
 	fs.writeFileSync(vdsJSON, JSON.stringify(vds, null, '\t'), 'utf8');
 }
