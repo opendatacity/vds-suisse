bc0d7ff2bc48955c8bcfb032031476bd885b534f
Micho <github@michael-kreil.de>
2014-04-04 13:24:38 +0200

    exporting js file

--- merge.js
+++ merge.js
@@ -16,6 +16,9 @@ if (usingCache && fs.existsSync(vdsJSON)) {
 	fs.writeFileSync(vdsJSON, JSON.stringify(vds, null, '\t'), 'utf8');
 }
 
+var cells = JSON.parse(fs.readFileSync(config.inputPath + 'cells.json', 'utf8'));
+cells.forEach(function (cell, index) { cell.index = index })
+
 // Positionen ausrechnen
 var positionJSON = config.cachePath + 'position.json';
 if (usingCache && fs.existsSync(positionJSON)) {
@@ -23,10 +26,53 @@ if (usingCache && fs.existsSync(positionJSON)) {
 	positions = JSON.parse(fs.readFileSync(positionJSON, 'utf8'));
 } else {
 	console.log('Calculate Position');
-	positions = require('position').import(config.inputPath + 'cells.json', vds, config);
-	fs.writeFileSync(positionJSON, JSON.stringify(positions, null, '\t'), 'utf8');
+	positions = require('position').import(cells, vds, config);
+	//fs.writeFileSync(positionJSON, JSON.stringify(positions, null, '\t'), 'utf8');
 }
 
+var telephoneEvents = vds.map(function (entry) {
+	if (entry.type == 'internet') return false;
+	return {
+		type: entry.type,
+		subtype: entry.subtype,
+		timeDuration: entry.timeDuration,
+		timeStart: entry.timeStart,
+		timeEnd: entry.timeEnd,
+		incoming: entry.data.incoming
+	}
+}).filter(function (entry) { return entry });
+events = events.concat(telephoneEvents);
+
+
+
+
+var data = {};
+
+data.cells = [];
+cells.forEach(function (cell) {
+	data.cells[cell.index] = {
+		x0: cell.x0,
+		y0: cell.y0,
+		x: cell.x,
+		y: cell.y,
+		acc: cell.acc,
+		index: cell.index
+	};
+})
+
+data.activities = positions.activities.map(function (activity) {
+	return {
+		index: activity.index,
+		time: activity.time,
+		cells: activity.cells.map(function (cell) { return cell.index })
+	}
+})
+
+data.timeStart = config.timeStart;
+
+data.events = events;
+
+fs.writeFileSync('../vds-vis/data/data.js', 'var data = ' + JSON.stringify(data, null, '\t'), 'utf8')
 
 
 
