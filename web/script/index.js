e816640290246af838af1bfad6023f3c80fe1e4a
Sebastian Vollnhals <sebastian@vollnhals.info>
2014-04-26 21:44:26 +0200

    bootstrap and polyfills for ie

--- web/script/index.js
+++ web/script/index.js
@@ -2,26 +2,21 @@ var title_long = 'Vorratsdatenspeicherung in der Schweiz';
 var where_to = 'http://apps.opendatacity.de/vds';
 
 $(document).ready(function () {
-	$('.share-pop.twitter').attr('href',
-			"https://twitter.com/intent/tweet?url=" + encodeURIComponent(where_to) + '&text=' + encodeURIComponent(title_long)
-	);
-	$('.share-pop.facebook').attr('href',
-			"http://www.facebook.com/sharer/sharer.php?u=" + encodeURIComponent(where_to) + '&t=' + encodeURIComponent(title_long)
-	);
-	$('.share-pop.google').attr('href',
-			"https://plus.google.com/share?url=" + encodeURIComponent(where_to) + '&text=' + encodeURIComponent(title_long)
-	);
-	$('.share-pop.adn').attr('href',
-			"https://alpha.app.net/intent/post?text=" +encodeURIComponent(title_long +' '+where_to)
-	);
+	
+	/* set urls for share buttons */
+	$('.share-pop.twitter').attr('href', "https://twitter.com/intent/tweet?url=" + encodeURIComponent(where_to) + '&text=' + encodeURIComponent(title_long));
+	$('.share-pop.facebook').attr('href', "http://www.facebook.com/sharer/sharer.php?u=" + encodeURIComponent(where_to) + '&t=' + encodeURIComponent(title_long));
+	$('.share-pop.google').attr('href',"https://plus.google.com/share?url=" + encodeURIComponent(where_to) + '&text=' + encodeURIComponent(title_long));
+	$('.share-pop.adn').attr('href',"https://alpha.app.net/intent/post?text=" +encodeURIComponent(title_long +' '+where_to));
 
-	/* share */
+	/* social media link */
 	$('.share-pop').click(function(evt){
 		evt.preventDefault();
 		window.open($(this).attr('href'), "share", "width=500,height=300,status=no,scrollbars=no,resizable=no,menubar=no,toolbar=no");
 		return false;
 	});
 
+	/* embed code */
 	$('#button-share').click(function(evt){
 		evt.preventDefault();
 		$('#container').toggleClass('show-share');
