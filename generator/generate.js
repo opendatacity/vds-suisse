91ec3b240d7bae899f36620284a555fe53123e97
Michael Kreil <github@michael-kreil.de>
2014-04-29 18:35:42 +0200

    starting multi language support generator

---
+++ generator/generate.js
@@ -0,0 +1,34 @@
+var fs = require('fs');
+
+var languages = ['de'];
+
+var frameHtml = fs.readFileSync('templates/frame.html', 'utf8');
+var indexHtml = fs.readFileSync('templates/index.html', 'utf8');
+
+languages.forEach(function (code) {
+	var lang = JSON.parse(fs.readFileSync('languages/'+code+'.json', 'utf8'));
+
+	translate(frameHtml, merge(lang.general, lang.frame), '../web/frame_'+code+'.html');
+
+	var indexFileName = (code == 'de') ? 'index.html' : 'index_'+code+'.html';
+	translate(indexHtml, merge(lang.general, lang.index), '../web/'+indexFileName);
+})
+
+function translate(html, language, file) {
+	html = html.replace(/\{\{.*?\}\}/g, function (key) {
+		var keyWord = key.substr(2, key.length-4);
+		if (language[keyWord] === undefined) {
+			console.error('Unknown key "'+keyWord+'"');
+			return key;
+		}
+		return language[keyWord];
+	})
+	fs.writeFileSync(file, html, 'utf8');
+}
+
+function merge(obj1, obj2) {
+	var obj = {};
+	Object.keys(obj1).forEach(function (key) { obj[key] = obj1[key] });
+	Object.keys(obj2).forEach(function (key) { obj[key] = obj2[key] });
+	return obj;
+}
