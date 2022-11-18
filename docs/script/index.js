9a49d64e240897f9dc2771b23aa5d53720be0b5e
signalwerk <sh@signalwerk.ch>
2022-11-18 12:55:38 +0100

    MOD: remove old social media networks

--- docs/script/index.js
+++ docs/script/index.js
@@ -6,8 +6,6 @@ $(document).ready(function () {
 	/* set urls for share buttons */
 	$('.share-pop.twitter').attr('href', "https://twitter.com/intent/tweet?url=" + encodeURIComponent(where_to) + '&text=' + encodeURIComponent(title_long));
 	$('.share-pop.facebook').attr('href', "http://www.facebook.com/sharer/sharer.php?u=" + encodeURIComponent(where_to) + '&t=' + encodeURIComponent(title_long));
-	$('.share-pop.google').attr('href',"https://plus.google.com/share?url=" + encodeURIComponent(where_to) + '&text=' + encodeURIComponent(title_long));
-	$('.share-pop.adn').attr('href',"https://alpha.app.net/intent/post?text=" +encodeURIComponent(title_long +' '+where_to));
 
 	/* social media link */
 	$('.share-pop').click(function(evt){
